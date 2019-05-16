b2f50d57607ddd7df4783ece501583a80607f027
kbub27 <kalebmerriman27@gmail.com>
2019-05-15 20:52:47 -0700

    fixed merge conflict

--- back_end/server.js
+++ back_end/server.js
@@ -1,5 +1,22 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
+
+var reservations = [
+  {
+    ID: "123456",
+    name: "john",
+    phone: "(206) 555-5555",
+    email: "john@john.com"
+  }
+];
+var waitlist = [
+  {
+    ID: "123456",
+    name: "john",
+    phone: "(206) 555-5555",
+    email: "john@john.com"
+  }
+];
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -24,7 +41,20 @@ app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
 
-var waitlist = [];
-var reservations = [];
+app.post("/api/tables", function(req, res) {
+  var newReservations = req.body;
 
+  newReservations.routeName = newReservations.name;
 
+  // console.log(newReservations);
+  if (reservations.length > 4) {
+    waitlist.push(newReservations);
+  } else {
+    reservations.push(newReservations);
+  }
+
+  res.json(newReservations);
+
+  console.log("reservations is: " + reservations.length);
+  console.log("waitlist is: " + waitlist.length);
+});
