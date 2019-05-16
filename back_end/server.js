5e0a25ba849622dc019f3ae9161624927c84f17f
Jordynn Otto <j_taylor92@yahoo.com>
2019-05-15 19:47:34 -0700

    added routes

--- back_end/server.js
+++ back_end/server.js
@@ -6,3 +6,21 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../front_end/home.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "../front_end/tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "../front_end/reserve.html"));
+});
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
